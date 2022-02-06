93131c2694d6aab67f921ff6a67b7d149b6bbebe
Alex Boxell <alexjboxell@gmail.com>
2022-02-06 16:39:06 +0000

    added jump animation for attacks

--- app/javascript/controllers/animation_controller.js
+++ app/javascript/controllers/animation_controller.js
@@ -1,10 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["attack", "star"]
+  static targets = ["attack", "star", "avatar", "opponent"]
 
   star() {
     console.log("Animate!");
     this.starTarget.classList.remove("invisible");
+  }
+
+  jump() {
+    this.avatarTarget.classList.add("avatar");
+    setTimeout(() => { this.avatarTarget.classList.remove("avatar"); }, 1000);
+  }
+
+  ojump() {
+    this.opponentTarget.classList.add("avatar");
+    setTimeout(() => { this.opponentTarget.classList.remove("avatar"); }, 1000);
   }
 }
